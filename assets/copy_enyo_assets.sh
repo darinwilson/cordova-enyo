f257907bfd5a9ad1e75030b8cd9be05e5c49bbc0
Darin Wilson <darinwilson@yahoo.com>
2013-11-05 10:22:35 -0800

    Fix before_prepare hook to work with Enyo's new directory structure

--- assets/copy_enyo_assets.sh
+++ assets/copy_enyo_assets.sh
@@ -11,4 +11,4 @@ cd $PROJECT_DIR/enyo
 
 # move the output of the Enyo tool to the project's www directory, which will
 # then be moved into each platform's build directory
-cp -Rf $PROJECT_DIR/enyo/deploy/enyo/* $PROJECT_DIR/www/
+cp -Rf $PROJECT_DIR/enyo/deploy/* $PROJECT_DIR/www/
